097f342002b662cb541908ef194a526e5ad94bad
Double_Shine <jhlcoder@gmail.com>
2021-10-11 13:43:43 +0900

    20211011

--- freeboard_frontend/src/components/unit/markets/detail/ProductDetail.styles.ts
+++ freeboard_frontend/src/components/unit/markets/detail/ProductDetail.styles.ts
@@ -19,3 +19,67 @@ export const SellerInformation = styled.div`
 export const Seller = styled.span`
 
 `
+
+export const UpdatedAt = styled.div`
+
+`
+
+export const Right =styled.div`
+
+`
+
+export const Address = styled.div`
+
+`
+
+export const Body = styled.div`
+
+`
+export const ProductHeader =styled.div`
+
+`
+export const Product = styled.div`
+
+`
+export const Remarks = styled.div`
+
+`
+
+export const Name = styled.div`
+
+`
+
+export const Like = styled.div`
+
+`
+
+export const PickedCount = styled.div`
+
+`
+
+export const ProductBody = styled.div`
+
+`
+export const Price = styled.div`
+
+`
+
+export const Image = styled.div`
+
+`
+
+export const Content = styled.div`
+
+`
+
+export const Tags = styled.div`
+
+`
+
+export const Map = styled.div`
+
+`
+
+export const Botton = styled.div`
+
+`
