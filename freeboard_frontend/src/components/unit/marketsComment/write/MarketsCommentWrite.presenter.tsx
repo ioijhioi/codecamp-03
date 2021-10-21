1ad5f68c850a283a28a689c01379e5ae22efde08
Double_Shine <jhlcoder@gmail.com>
2021-10-21 09:52:03 +0900

    123

--- freeboard_frontend/src/components/unit/marketsComment/write/MarketsCommentWrite.presenter.tsx
+++ freeboard_frontend/src/components/unit/marketsComment/write/MarketsCommentWrite.presenter.tsx
@@ -1,55 +1,14 @@
 import {
     Wrapper,
-    CommentIcon,
-    Header,
-    Star,
-    ContentWrapper,
-    Content,
-    BottomWrapper,
-    ContentLength,
-    Button,
+    Line,
+    
 } from "./MarketsCommentWrite.styles"
 
-export default function MarketsCommentUI (props){
+export default function MarketsCommentUI (){
 
     return (
         <Wrapper>
-          {!props.isEdit && (
-            <>
-              <CommentIcon src="/images/comment/CommentIcon.png" />
-              <span>댓글</span>
-            </>
-          )}
-          <CommentIcon>
-            <Header
-              placeholder="작성자"
-              readOnly={Boolean(props.el?.writer)}
-              defaultValue={props.el?.writer}
-              onChange={props.onChangeMyWriter}
-            />
-            <Header
-              placeholder="비밀번호"
-              type="password"
-              onChange={props.onChangeMyPassword}
-            />
-            <Star onChange={props.onChangeStar} />
-          </CommentIcon>
-          <ContentWrapper>
-            <Content
-              defaultValue={props.el?.contents}
-              onChange={props.onChangeMyContents}
-              placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
-            />
-            <BottomWrapper>
-              <ContentLength>0/100</ContentLength>
-              <Button
-                id={props.el?._id}
-                onClick={props.isEdit ? props.onClickUpdate : props.onClickWrite}
-              >
-                {props.isEdit ? "수정하기" : "등록하기"}
-              </Button>
-            </BottomWrapper>
-          </ContentWrapper>
+            <Line/>
         </Wrapper>
       );
 }
